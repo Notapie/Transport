fb48ab368eac8f751b09d23d87c05493ef4dc176
notapie <notapie@yandex.ru>
2021-09-18 17:34:31 +0300

    JsonReader structure created

--- service/json_reader.h
+++ service/json_reader.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include "json/json.h"
+#include "request_handler.h"
+#include "transport_catalogue/transport_catalogue.h"
+
+namespace transport_catalogue::service {
+
+    class JsonReader {
+    public:
+        JsonReader(const TransportCatalogue& db);
+        void ReadQueries(std::istream& input, std::ostream& output) const;
+
+    private:
+        const TransportCatalogue& db_;
+
+        void BaseRequests(json::Array&) const;
+        void StatRequests(json::Array&, std::ostream&) const;
+
+    };
+
+} // namespace transport_catalogue::service
