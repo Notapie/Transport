b6aee9d59c5c44559a6b57689ed998f2b4e906f7
notapie <notapie@yandex.ru>
2021-09-26 14:42:33 +0300

    render settings structure created

--- service/map_renderer/map_renderer.h
+++ service/map_renderer/map_renderer.h
@@ -6,6 +6,27 @@
 
 namespace transport_catalogue::service {
 
+    struct RenderSettings {
+        double width = 0.0;
+        double height = 0.0;
+
+        double padding = 0.0;
+
+        double linew_width = 0.0;
+        double stop_radius = 0.0;
+
+        int bus_label_font_size = 0;
+        geo::Coordinates bus_label_offsets;
+
+        int stop_label_font_size = 0;
+        geo::Coordinates stop_label_offsets;
+
+        svg::Color underlayer_color;
+        double underlayer_width = 0.0;
+
+        std::vector<svg::Color> color_palette;
+    };
+
     class MapRenderer {
 
     };
