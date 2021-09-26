c47ab8c006c280e6854912cdb95ae8bd3f4a6f5b
notapie <notapie@yandex.ru>
2021-09-27 00:18:47 +0300

    SphereProjector class added to map_renderer

--- service/map_renderer/map_renderer.cpp
+++ service/map_renderer/map_renderer.cpp
@@ -1,1 +1,14 @@
 #include "map_renderer.h"
+
+namespace transport_catalogue::service {
+
+    namespace detail {
+
+        svg::Point SphereProjector::operator()(geo::Coordinates coords) const {
+            return {(coords.lng - min_lon_) * zoom_coeff_ + padding_,
+                    (max_lat_ - coords.lat) * zoom_coeff_ + padding_};
+        }
+
+    } // namespace detail
+
+} // namespace transport_catalogue::service
--- service/map_renderer/map_renderer.h
+++ service/map_renderer/map_renderer.h
@@ -1,10 +1,73 @@
 #pragma once
 
+#include <algorithm>
+
 #include "svg/svg.h"
 #include "transport_catalogue/domain.h"
 #include "geo/geo.h"
 
 namespace transport_catalogue::service {
+
+    namespace detail {
+
+        inline const double EPSILON = 1e-6;
+        bool IsZero(double value) {
+            return std::abs(value) < EPSILON;
+        }
+
+        class SphereProjector {
+        public:
+            template <typename PointInputIt>
+            SphereProjector(PointInputIt points_begin, PointInputIt points_end, double max_width,
+                            double max_height, double padding)
+                    : padding_(padding) {
+                if (points_begin == points_end) {
+                    return;
+                }
+
+                const auto [left_it, right_it]
+                = std::minmax_element(points_begin, points_end, [](auto lhs, auto rhs) {
+                    return lhs.lng < rhs.lng;
+                });
+                min_lon_ = left_it->lng;
+                const double max_lon = right_it->lng;
+
+                const auto [bottom_it, top_it]
+                = std::minmax_element(points_begin, points_end, [](auto lhs, auto rhs) {
+                    return lhs.lat < rhs.lat;
+                });
+                const double min_lat = bottom_it->lat;
+                max_lat_ = top_it->lat;
+
+                std::optional<double> width_zoom;
+                if (!IsZero(max_lon - min_lon_)) {
+                    width_zoom = (max_width - 2 * padding) / (max_lon - min_lon_);
+                }
+
+                std::optional<double> height_zoom;
+                if (!IsZero(max_lat_ - min_lat)) {
+                    height_zoom = (max_height - 2 * padding) / (max_lat_ - min_lat);
+                }
+
+                if (width_zoom && height_zoom) {
+                    zoom_coeff_ = std::min(*width_zoom, *height_zoom);
+                } else if (width_zoom) {
+                    zoom_coeff_ = *width_zoom;
+                } else if (height_zoom) {
+                    zoom_coeff_ = *height_zoom;
+                }
+            }
+
+            svg::Point operator()(geo::Coordinates coords) const;
+
+        private:
+            double padding_;
+            double min_lon_ = 0;
+            double max_lat_ = 0;
+            double zoom_coeff_ = 0;
+        };
+
+    } // namespace detail
 
     struct RenderSettings {
         double width = 0.0;
@@ -31,4 +94,4 @@ namespace transport_catalogue::service {
 
     };
 
-}
+} // namespace transport_catalogue::service
