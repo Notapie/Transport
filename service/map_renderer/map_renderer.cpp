597e07fb2a706348319385ee67c2e964f2eea1ba
notapie <notapie@yandex.ru>
2021-09-28 14:36:49 +0300

    IsZero function and StopPtrComparator definitions created

--- service/map_renderer/map_renderer.cpp
+++ service/map_renderer/map_renderer.cpp
@@ -1,12 +1,22 @@
 #include "map_renderer.h"
 
+#include <set>
+
 namespace transport_catalogue::service {
 
     namespace detail {
 
+        bool IsZero(double value) {
+            return std::abs(value) < EPSILON;
+        }
+
         svg::Point SphereProjector::operator()(geo::Coordinates coords) const {
             return {(coords.lng - min_lon_) * zoom_coeff_ + padding_,
                     (max_lat_ - coords.lat) * zoom_coeff_ + padding_};
+        }
+
+        bool StopPtrComparator::operator()(const domain::Stop *lhs, const domain::Stop *rhs) const {
+            return lhs->name > rhs->name;
         }
 
     } // namespace detail
